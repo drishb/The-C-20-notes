5efc70a5ebba82c26cd5445242a118d4f1ad227c
drishti <67603818+drishb@users.noreply.github.com>
2022-08-07 16:57:29 +0530

    adding comments

--- 4.VariablesAndDatatypes/4.2NumberSystems/main.cpp
+++ 4.VariablesAndDatatypes/4.2NumberSystems/main.cpp
@@ -7,11 +7,13 @@ int main(){
    int number2 = 017; // Octal
    int number3 = 0x0F; // Hexadecimal
    int number4 = 0b00001111; // Binary
+   // for octal add 0, hex add ox, bin add 0b before th number
 
    std::cout << "number1 : " << number1 << std::endl;
    std::cout << "number2 : " << number2 << std::endl;
    std::cout << "number3 : " << number3 << std::endl;
    std::cout << "number4 : " << number4 << std::endl;
+   //the out put for all wil be 15 as these are different representations of the same number that is 15.
    
     return 0;
 }
